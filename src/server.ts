1e8377847f69909ca5939ffc0a213f92b47e2b72
PashaDev2 <pasha.yakubovsky@lynksen.com>
2024-08-10 19:13:25 +0300

    +update ws

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,8 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
-        // origin: /^(http:\/\/localhost:5173|https:\/\/camelface\.vercel\.app)$/i,
-        origin: "https://camelface.vercel.app",
+        origin: /^(http:\/\/localhost:5173|https:\/\/camelface\.vercel\.app)$/i,
+        // origin: "https://camelface.vercel.app",
         methods: ["GET", "POST"],
     },
 });
